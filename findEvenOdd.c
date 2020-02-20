309db84721faafbe07bdd0a5d75abf5b13c00227
Md. Rakib Trofder <48678659+rakib3004@users.noreply.github.com>
2020-02-19 20:12:33 -0800

    Update findEvenOdd.c

--- findEvenOdd.c
+++ findEvenOdd.c
@@ -7,14 +7,12 @@ printf("enter the value of a&b:");
 scanf("%d%d",&a,&b);
 b=a%2;
 if(b==0)
-{
+{   
     printf("the number is even");
-
 }
     else
     {
         printf("the number is odd");
-
     }
     return 0;
 }
