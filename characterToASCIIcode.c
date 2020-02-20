1678446d570a27478268210438b3ca77e2fa9f68
Md. Rakib Trofder <48678659+rakib3004@users.noreply.github.com>
2020-02-19 20:22:27 -0800

    Update and rename iit100.c to characterToASCIIcode.c
    This program is convert character to it's ASCII code . '1' to 49 , 'A' to 65 ,'B' to 66 .

--- characterToASCIIcode.c
+++ characterToASCIIcode.c
@@ -2,17 +2,10 @@
 #include<conio.h>
 
 int main(void)
-
 {
-
     char ch;
-
     printf("enter a character :");
     ch=getche();
     printf("\nIts ASCII code is %d",ch);
-
     return 0;
-
-
-
 }
