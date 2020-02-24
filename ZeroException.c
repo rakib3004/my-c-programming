74b1936bc11d20b82fff03aa99f8686b2308f46d
Md. Rakib Trofder <48678659+rakib3004@users.noreply.github.com>
2020-02-24 19:32:54 +0600

    Update and rename iit128.c to ZeroException.c

--- ZeroException.c
+++ ZeroException.c
@@ -19,7 +19,7 @@ int main(void)
         {
         printf("%d\n",i/j);
         }
-        if(j==0)
+        if(j==0)//nothing can divided by zero .
 
             {
                 printf("This is not possible");
