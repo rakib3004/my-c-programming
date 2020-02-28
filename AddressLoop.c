fbb3f550b5792633dc8c349b70f57343924bc8dc
Md. Rakib Trofder <48678659+rakib3004@users.noreply.github.com>
2020-02-28 12:27:12 +0600

    Update and rename iit39.c to AddressLoop.c

--- AddressLoop.c
+++ AddressLoop.c
@@ -4,7 +4,8 @@ int main()
 {
 
     int a;
-
+//this is a loop of address 
+    //where you can find adress in serially .
     for(a=1;a<=15;a++)
     {
         printf("house no:%d,road no:%d,flat no:%d\n",a+2,a+7,a);
