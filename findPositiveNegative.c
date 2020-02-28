4599d49089644f614ae0243aabcb29fb23d44100
Md. Rakib Trofder <48678659+rakib3004@users.noreply.github.com>
2020-02-28 17:34:43 +0600

    Update and rename iit46.c to findPositiveNegative.c

--- findPositiveNegative.c
+++ findPositiveNegative.c
@@ -6,15 +6,16 @@ int num;
 
 printf("Enter an integer: ");
 scanf("%d",&num);
-
+// enter number 
 if(num<0)
 {
-
+// print number is negative 
     printf("Number is negative:");
 }
 else
 {
-    printf("Number is non-negative:");
+   // print number is non-negative
+ printf("Number is non-negative:");
 }
 return 0;
 }
