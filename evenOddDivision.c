8e0508caea6c04e3bf19962e1e29e98610a0277b
Md. Rakib Trofder <48678659+rakib3004@users.noreply.github.com>
2020-02-29 20:52:58 +0600

    Update and rename iit53.c to evenOddDivision.c

--- evenOddDivision.c
+++ evenOddDivision.c
@@ -1,10 +1,13 @@
 #include<stdio.h>
 int main(void)
 {
-
+//division
 printf("%d\n",5/2) ;
+  //modulus
 printf("%d\n",5%2) ;
+  //division
 printf("%d\n",5/2);
+    //modulus
 printf("%d\n",4%2) ;
 
 return 0;
