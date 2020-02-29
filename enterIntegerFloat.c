555fca1eee9c891fedb669af07702971a36af1b0
Md. Rakib Trofder <48678659+rakib3004@users.noreply.github.com>
2020-02-29 20:50:14 +0600

    Update and rename iit52.c to enterIntegerFloat.c

--- enterIntegerFloat.c
+++ enterIntegerFloat.c
@@ -4,10 +4,10 @@ int main (void)
 
     int num;
     float f;
-
+//Enter an integer
     printf("Enter an integer : ");
     scanf("%d",&num);
-
+//Enter a floating point number
     printf("Enter a floating point number:");
     scanf("%f",&f);
 
