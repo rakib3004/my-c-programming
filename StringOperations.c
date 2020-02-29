c8fb4fe38b480139d300783e6b4c1cc31f5d5879
Md. Rakib Trofder <48678659+rakib3004@users.noreply.github.com>
2020-02-29 20:49:07 +0600

    Update and rename iit51.c to StringOperations.c

--- StringOperations.c
+++ StringOperations.c
@@ -1,3 +1,5 @@
+
+// String Operations 
 #include<stdio.h>
 #include<string.h>
  int main ()
@@ -6,10 +8,13 @@
       char str2[12]="World" ;
        char str3[12];
        int len;
+  //copy string
        strcpy(str3,str1);
        printf("strcpy(str3,str1): %s\n",str3);
+  //concatation string
        strcat(str1,str2);
         printf("strcat(str1,str2): %s\n",str1);
+  //find the length of string 
         len=strlen(str1) ;
         printf("strlen(str1):%d\n",len);
 
