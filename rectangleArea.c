2ccda7f67f3f5e83021bf23a5354195a65b7c202
Md. Rakib Trofder <48678659+rakib3004@users.noreply.github.com>
2020-02-29 20:54:32 +0600

    Update and rename iit54.c to rectangleArea.c

--- rectangleArea.c
+++ rectangleArea.c
@@ -3,13 +3,18 @@
 
 int main(void)
 {
-
+//variable declearation
+    
     int len,width,area;
+    //enter length
     printf("enter length:") ;
     scanf("%d",&len) ;
+    //enter width
     printf("enter width :") ;
     scanf("%d",&width) ;
+    //caculate area of rectangle
     area=len*width;
+    // print area 
     printf("Area is %d*%d=%d",len,width,area);
     return 0;
 
