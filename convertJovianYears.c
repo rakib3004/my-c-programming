4774acb3f603a45337b6fa872de65b119b271464
Md. Rakib Trofder <48678659+rakib3004@users.noreply.github.com>
2020-03-01 08:31:18 +0600

    Update and rename iit56.c to convertJovianYears.c

--- convertJovianYears.c
+++ convertJovianYears.c
@@ -6,6 +6,7 @@ int main()
     printf("Enter number of Earth days:");
     scanf("%f",&e_days);
     j_years = e_days/(365.0*12.0) ;
+//print the value of jovian years
     printf("equivalent jovian years: %f",j_years);
 
     return 0;
