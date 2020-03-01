224af2d75a69b58b2a6eba162984e56d7a0fbc7d
Md. Rakib Trofder <48678659+rakib3004@users.noreply.github.com>
2020-03-01 12:27:45 +0600

    Update and rename iit63.c to returnFromFunction.c

--- returnFromFunction.c
+++ returnFromFunction.c
@@ -4,11 +4,13 @@
      {
 
          int num;
+          // retuen value  from function 
          num = function();
          printf("%d",num);
          return 0;
      }
  int   function(void)
  {
+      // this function returns 16
      return 16;
  }
