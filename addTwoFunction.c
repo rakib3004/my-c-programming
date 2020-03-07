816370814f964c230366208c44df0f37bea1e47d
Md. Rakib Trofder <48678659+rakib3004@users.noreply.github.com>
2020-03-08 05:44:39 +1200

    Update and rename iit68.c to addTwoFunction.c

--- addTwoFunction.c
+++ addTwoFunction.c
@@ -7,10 +7,12 @@
              sum(32,4);
                  sum(2,54);
                  sum(12,43);
+                  // use this function 3 times
 
                  return 0;
          }
          void sum(int x,int y)
          {
+                  //function thst add two numbers
              printf("%d\n",x+y);
          }
