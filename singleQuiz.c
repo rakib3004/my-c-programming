362b73465cd82bea3b33a1bb7e243c5a8a2d8285
Md. Rakib Trofder <48678659+rakib3004@users.noreply.github.com>
2020-03-08 05:50:06 +1200

    Update and rename iit72.c to singleQuiz.c

--- singleQuiz.c
+++ singleQuiz.c
@@ -4,10 +4,13 @@ int main(void)
 {
     int answer;
     printf("What is 10+14?");
+    // Ask a qustion
     scanf("%d",&answer);
     if (answer ==10+14)
+        // say rihgt 
     printf("Right!");
     else
+        //else say wrong
         printf("Wrong");
     return 0;
 }
