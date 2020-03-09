66583b52fed48951a6f31302f87934ef99bb054c
Md. Rakib Trofder <48678659+rakib3004@users.noreply.github.com>
2020-03-10 08:10:01 +1200

    update this continuous quiz by rename and add comment
            ask the question with an iterator
    check answer and
    print welcome or sorry

--- ContinuousQuiz.c
+++ ContinuousQuiz.c
@@ -6,16 +6,18 @@ int main(void)
     int answer,counter;
     for(counter=1;counter<5;counter=counter+1)
     {
+        // ask questionn with iterator
         printf("what is %d+%d\n",counter,counter);
         scanf("%d",&answer);
-
+// verify answer
     if(answer=counter+counter)
     {
+        // say welcome if the answer is correct
         printf("welcome,Your are right");
-
     }
     else
         {
+                // say sorry if the answer is correct
        printf("sorry, You are  wrong");
        printf("the number is %d",counter+counter);
     }
