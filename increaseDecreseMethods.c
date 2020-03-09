9d008c4d5865df24876540b52378546986dc687e
Md. Rakib Trofder <48678659+rakib3004@users.noreply.github.com>
2020-03-10 08:42:12 +1200

    Update and rename iit89.c to increaseDecreseMethods.c
    show the functionality of plus plus and minus minus method

--- increaseDecreseMethods.c
+++ increaseDecreseMethods.c
@@ -3,9 +3,15 @@ int main()
 {
 int i;
 i=5;
+  // value is increasing 
 i++;
+  // print value 
 printf("%d\n",i);
+  //  // value is decreasing 
+
 i--;
+  // print value 
+  
 printf("%d\n",i);
 return 0;
 }
