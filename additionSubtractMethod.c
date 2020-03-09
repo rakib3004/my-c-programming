95f92449698458681502d27477f278ea415d22df
Md. Rakib Trofder <48678659+rakib3004@users.noreply.github.com>
2020-03-10 08:43:57 +1200

    Update and rename iit90.c to additionSubtractMethod.c

--- additionSubtractMethod.c
+++ additionSubtractMethod.c
@@ -5,10 +5,11 @@ int main(void)
  int a,b;
 
  a=1129;
+ // adding value
  a=a+12;
 
  b=a;
-
+// subtract value
  b=b-62;
       printf("the both number is a and b : %d and %d",a,b);
       return 0;
